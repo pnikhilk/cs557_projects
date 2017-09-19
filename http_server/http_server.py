6027d7589de3c3185640dcd44526972e86a69796
Nikhil <pnikhilk@gmail.com>
2017-09-19 15:24:29 -0400

    error handling and thread safe file access count

--- http_server/http_server.py
+++ http_server/http_server.py
@@ -23,45 +23,48 @@ class HTTP_Server():
                                "\r\nContent-Type: {}\r\nContent-Length: {}" +\
                                "\r\n\n{}"
     rfc7231_date_template = "%a, %d %b %Y %H:%M:%S GMT"
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.ERROR)
 
     def __init__(self, host='', port=0):
         self.host = host
         self.port = port
-        self.server_name = 'ArchServer'
+        self.server_name = 'HTTP Server/Python 2.7'
         self.logger = logging.getLogger(__name__)
         self.file_access_count = {}
+        self._set_files()
         self._set_mime_types()
 
     def listen(self, client_socket, client_ip):
             self.logger.info("Connection request from client %s" %
                              str(client_ip))
             msg = client_socket.recv(1024).decode('ascii')
-            requested_file = re.search(r'GET /(.+) HTTP/1..*', msg).group(1)
-            self.logger.debug("Requested file from client: %s" %
-                              requested_file)
-            reply = self._make_response(**{'requested_file': requested_file})
-            self.logger.debug('Reply message : %s' % reply[:50])
-            bytes_sent = client_socket.send(reply)
-            print("Bytes sent: %s" % bytes_sent)
-            client_socket.close()
-            if self.status_codes[404] not in reply:
-                print("/%s|%s|%d|%d" % (requested_file, client_ip[0],
-                      client_ip[1], self.file_access_count[requested_file]))
+
+            parsed_request = re.search(r'GET /(.+) HTTP/1..*', msg)
+            if parsed_request:
+                requested_file = parsed_request.group(1)
+                self.logger.debug("Requested file from client: %s" %
+                                  requested_file)
+                reply = self._make_response(**{'requested_file': requested_file})
+                self.logger.debug('Reply message : %s' % reply[:50])
+                bytes_sent = client_socket.send(reply)
+                self.logger.debug("Bytes sent: %s" % bytes_sent)
+                client_socket.close()
+                if self.status_codes[404] not in reply:
+                    with self.file_access_count[requested_file][0]:
+                        count = self.file_access_count[requested_file][1] + 1
+                        self.file_access_count[requested_file][1] = count
+                        print("/%s|%s|%d|%d" % (requested_file, client_ip[0],
+                                                client_ip[1], count))
 
     def _make_response(self, **kwargs):
         requested_file = kwargs['requested_file']
         file_path = os.path.join(self.resource_dir, requested_file)
         response = ""
+        file_content = ""
         if os.path.exists(file_path):
-            file_content = ""
             with open(file_path, 'rb') as fin:
                 file_content = fin.read()
             response_code = self.status_codes[200]
-            if requested_file in self.file_access_count:
-                self.file_access_count[requested_file] += 1
-            else:
-                self.file_access_count[requested_file] = 1
         else:
             return self.response_status.format(self.status_codes[404])
 
@@ -90,15 +93,19 @@ class HTTP_Server():
                     for ext in ext_list.split():
                         self.mime_types[ext] = mime_type
 
+    def _set_files(self):
+        for f in os.listdir(self.resource_dir):
+            self.file_access_count[f] = [threading.Lock(), 0]
+
     def run_server(self):
         try:
             server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             server_socket.bind((self.host, self.port))
-            self.host = socket.gethostbyname(socket.gethostname())
+            self.host = socket.gethostbyaddr(socket.gethostname())[0]
             self.port = server_socket.getsockname()[1]
             self.logger.debug("Server bind at address %s:%s." %
                               (self.host, self.port))
-            print("Host: %s, Port:%s" % (self.host, self.port))
+            print("Server is started on %s:%s" % (self.host, self.port))
             server_socket.listen(5)
             self.logger.info("Server listening at %s:%s" %
                              (self.host, self.port))
@@ -109,7 +116,9 @@ class HTTP_Server():
                                                  args=thread_args)
                 client_thread.start()
         except Exception as e:
-            print(e)
+            self.logger.error(e)
+        except KeyboardInterrupt:
+            pass
         finally:
             server_socket.close()
             self.logger.info("Server connection closed.")
