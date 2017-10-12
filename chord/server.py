e3fd242a9da7b6767cc7619041a725c9da0fa388
Nikhil <pnikhilk@gmail.com>
2017-10-12 17:19:25 -0400

    read/write file and few changes in in range

--- chord/server.py
+++ chord/server.py
@@ -1,10 +1,12 @@
 #! /usr/bin/python
-import hashlib
+from hashlib import sha256
 import logging
+import os
 import pdb
 import socket
 import sys
 sys.path.append("lib/gen-py/chord")
+sys.path.append("gen-py/chord")
 
 from thrift.transport import TSocket, TTransport
 from thrift.protocol import TBinaryProtocol
@@ -18,87 +20,176 @@ class ChordHandler:
     logging.basicConfig(level=logging.DEBUG)
 
     def __init__(self):
-        self.logger = logging.getLogger(__name__)
-        sha256 = hashlib.sha256()
         ip = socket.gethostbyname(socket.gethostname())
         ip_port = ip + ":" + sys.argv[1]
-        self.logger.debug("ip_port %s" % ip_port)
-        sha256.update(ip_port)
-        self.node = ttypes.NodeID(sha256.hexdigest(), ip, sys.argv[1])
-        self.finger_table = {}
+        self.logger = logging.getLogger(__name__)
+        self.max_node = ""
+        filehash = self._get_hash(ip_port)
+        self.node = ttypes.NodeID(filehash, ip, sys.argv[1])
+        self.finger_table = None
+        self.file_table = {}
 
     @staticmethod
-    def _in_range(id1, id2, key):
+    def _get_hash(arg):
+        sha = sha256()
+        sha.update(arg)
+        return sha.hexdigest()
+
+    @staticmethod
+    def _in_range(id1, id2, key, preced=False):
         """
         Find if key belongs to range (id1, id2)
         """
-        init_node = "0" * 64
-        if (key > id1 or key > init_node) and (key < id2):
-            return True
+        if (id1 > id2):
+            if (not preced):
+                if (key <= id2 or key > id1):
+                    return True
+            elif (key < id2 or key > id1):
+                return True
         else:
-            return False
+            if (not preced):
+                if (key > id1) and (key <= id2):
+                    return True
+            elif (key > id1 and key < id2):
+                return True
+        return False
+
+    @staticmethod
+    def _client_succ(node):
+        socket = TSocket.TSocket(node.ip, node.port)
+        transport = TTransport.TBufferedTransport(socket)
+        protocol = TBinaryProtocol.TBinaryProtocol(transport)
+        client = FileStore.Client(protocol)
+        transport.open()
+        return client.getNodeSucc()
+
+    @staticmethod
+    def _client_findpred(node, key):
+        socket = TSocket.TSocket(node.ip, node.port)
+        transport = TTransport.TBufferedTransport(socket)
+        protocol = TBinaryProtocol.TBinaryProtocol(transport)
+        client = FileStore.Client(protocol)
+        transport.open()
+        return client.findPred(key)
 
     def writeFile(self, rFile):
+        content = rFile.content
+        meta = rFile.meta
+        self.logger.debug("content:%s,meta:%s" % (content, meta))
+        filehash = self._get_hash(meta.owner + ":" + meta.filename)
+        # pdb.set_trace()
+        content_hash = self._get_hash(content)
+        successor = self.findSucc(filehash)
+        if successor.id != self.node.id:
+            raise(ttypes.SystemException("This node does not own this file."))
+        if filehash not in self.file_table:
+            meta.contentHash = content_hash
+            meta.version = 0
+            self.file_table[filehash] = meta
+        else:
+            self.file_table[filehash].version += 1
+            self.file_table[filehash].contentHash = content_hash
+
+        with open(meta.filename, "wb") as fout:
+            fout.write(content)
         pass
 
-    def readFile(self, rFile):
-        pass
+    def readFile(self, filename, owner):
+        """
+        Retrun requested file
+        :param filename: Name of file
+        :param owner: Owner of file
+        :return rFile: RFile object
+        """
+        self.logger.debug("Readfile fname:%s, owner:%s" % (filename, owner))
+        filehash = self._get_hash(owner + ":" + filename)
+        # sha256.update(owner + ":" + filename)
+        # filehash = sha256.hexdigest()
+        if (filehash in self.file_table) and os.path.exists(filename):
+            with open(filename, "rb") as fin:
+                content = fin.read()
+            rFile = ttypes.RFile(self.file_table[filehash], content)
+            return rFile
+        else:
+            if not os.path.exists(filename):
+                raise ttypes.SystemException("Requested file does not exists.")
+            else:
+                raise ttypes.SystemException("File does not belong to thes node.")
 
     def setFingertable(self, node_list):
         """
         Set finger table for current server node.
         :param node_list: List containing NodeID objects
         """
+        self.max_node = max(node_list, key=lambda node: node.id)
         for i, node in enumerate(node_list, start=1):
+            if not self.finger_table:
+                self.finger_table = {}
             self.finger_table[i] = node
 
         # for k, v in self.finger_table.items():
-        #     self.logger.debug(k,v)
+        #     print(k, v)
 
     def findSucc(self, key):
         """
         Find successor node of key
         :param key: SHA256 hash key of a file
+        :return node: successor NodeID object
         """
         node = self.findPred(key)
         self.logger.debug(node)
         if node != self.node:
-            socket = TSocket.TSocket(node.ip, node.port)
-            transport = TTransport.TBufferedTransport(socket)
-            protocol = TBinaryProtocol.TBinaryProtocol(transport)
-            client = FileStore.Client(protocol)
-            transport.open()
-            return client.getNodeSucc()
+            return self._client_succ(node)
+            # socket = TSocket.TSocket(node.ip, node.port)
+            # transport = TTransport.TBufferedTransport(socket)
+            # protocol = TBinaryProtocol.TBinaryProtocol(transport)
+            # client = FileStore.Client(protocol)
+            # transport.open()
+            # return client.getNodeSucc()
         else:
+            self.logger.debug("returning self")
             return self.getNodeSucc()
 
     def findPred(self, key):
         """
         Find predecessor node of key
         :param key: SHA256 hash key of a file
+        :return node: Predecessor NodeID object
         """
         node = self
-        self.logger.debug("in pred")
-        self.logger.debug("ke %s" % key)
-        self.logger.debug("id %s" % node.node.id)
-        self.logger.debug("fin %s" % node.finger_table[1].id)
-        # pdb.set_trace()
-        while not self._in_range(node.node.id, node.finger_table[1].id, key):
-            self.logger.debug('in while')
-            for i in xrange(256, 0, -1):
-                self.logger.debug("id %s" % node.node.id)
-                self.logger.debug("fin %s" % node.finger_table[i].id)
-                if self._in_range(node.node.id, key, node.finger_table[i].id):
-                    return node.finger_table[i]
-        return node.node
+        if self.finger_table:
+            self.logger.debug("ke %s" % key)
+            self.logger.debug("id %s" % node.node.id)
+            self.logger.debug("fin[1] %s" % node.finger_table[1].id)
+
+            # pdb.set_trace()
+            if not self._in_range(node.node.id, node.finger_table[1].id, key):
+                for i in xrange(256, 0, -1):
+                    # self.logger.debug("id %s" % node.node.id)
+                    # self.logger.debug("fin %s" % node.finger_table[i].id)
+                    fing_node = node.finger_table[i]
+                    if self._in_range(node.node.id, key, fing_node.id, True):
+                        succ_node = self._client_succ(node.finger_table[i])
+                        if self._in_range(fing_node.id, succ_node.id, key):
+                            return node.finger_table[i]
+                        else:
+                            self.logger.debug("calling findsucc")
+                            return self._client_findpred(fing_node, key)
+            return node.node
+        else:
+            raise ttypes.SystemException("No finger table exist for current node.")
 
     def getNodeSucc(self):
         """
         Get immediate successor of node
+        :return node: immediate successor NodeID object of current node
         """
         self.logger.debug(sys.argv[1])
-        self.logger.debug(self.finger_table[1])
-        return self.finger_table[1]
+        # pdb.set_trace()
+        if self.finger_table[1]:
+            return self.finger_table[1]
+        else:
+            raise ttypes.SystemException("No such node in finger table.")
 
 
 if __name__ == "__main__":
@@ -113,5 +204,5 @@ if __name__ == "__main__":
 
     server = TServer.TSimpleServer(processor, transport, tfactory, pfactory)
 
-    print("Starting server...")
+    print("Starting server at %s:%s" % (handler.node.ip, handler.node.port))
     server.serve()
